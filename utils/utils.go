136a1b29a1a4fe7e149fd0371e33c803fbe9c62a
Yaron <horsky.yaron@gmail.com>
2023-09-21 15:38:07 +0300

    changed const variables to global variables in the utils file

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,11 @@
 package utils
 
-const (
-	SHOAM_DIR = "/home/yaron/projects/shoam"
-	DB_DIR    = SHOAM_DIR + "/db"
-	SCRIPTS_DIR    = SHOAM_DIR + "/scripts"
-	LINKS_DIR    = SHOAM_DIR + "/links"
+import "os"
+
+var SHOAM_DIR = os.Getenv("SHOAM_DIR")
+
+var (
+	DB_DIR      = SHOAM_DIR + "/db"
+	SCRIPTS_DIR = SHOAM_DIR + "/scripts"
+	LINKS_DIR   = SHOAM_DIR + "/links"
 )
